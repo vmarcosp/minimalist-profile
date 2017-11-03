85a2c12d2c3af292f542ab37e29bff97ff3e66bd
Marcos Vinicius <contato@vmarcosp.com.br>
2017-11-03 18:44:20 -0200

    Created task for build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,50 @@ import { create } from 'browser-sync'
 import sass from 'gulp-sass'
 import sourcemaps from 'gulp-sourcemaps'
 import rename from 'gulp-rename'
+import concatCss from 'gulp-concat-css'
+import uglifyCss from 'gulp-uglifycss'
+import gulpCopy from 'gulp-copy'
+import gulpReplace from 'gulp-replace'
+import htmlReplace from 'gulp-html-replace'
 
 const browserSync = create()
+
+const FONT_AWESOME_PATH = 'node_modules/font-awesome'
+const DIST_PATH = 'dist/'
+const ASSETS_PATH = `${DIST_PATH}/assets`
+
+gulp.task('build:css', ['build:sourcemap'], () =>
+  gulp.src(['css/style.css', `${FONT_AWESOME_PATH}/css/font-awesome.min.css`])
+    .pipe(concatCss('css/all.min.css'))
+    .pipe(uglifyCss())
+    .pipe(gulp.dest(ASSETS_PATH))
+)
+
+gulp.task('build:sourcemap', ['sass'], () =>
+  gulp.src('css/style.css.map')
+    .pipe(gulp.dest(`${ASSETS_PATH}/css`))
+)
+
+gulp.task('build:fa', () =>
+  gulp.src([`${FONT_AWESOME_PATH}/fonts/fontawesome-webfont.*`, `${FONT_AWESOME_PATH}/fonts/FontAwesome.otf`])
+    .pipe(gulp.dest(`${ASSETS_PATH}/fonts`, {}))
+)
+
+gulp.task('build:replace-fa', ['build:fa'], () =>
+  gulp.src(`${ASSETS_PATH}/css/all.min.css`)
+    .pipe(gulpReplace('../../node_modules/font-awesome', '..'))
+    .pipe(gulp.dest(`${ASSETS_PATH}/css`))
+)
+
+gulp.task('build:html', () =>
+  gulp.src('index.html')
+    .pipe(htmlReplace({
+      'css': './assets/css/all.min.css'
+    }))
+    .pipe(gulp.dest(DIST_PATH))
+)
+
+gulp.task('build', ['build:html', 'build:css', 'build:replace-fa'])
 
 gulp.task('dev', ['sass'], () => {
 
@@ -17,8 +59,8 @@ gulp.task('dev', ['sass'], () => {
 gulp.task('sass', () =>
   gulp.src('./scss/all.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', console.error))
-    .pipe(rename('style.min.css'))
+    .pipe(sass().on('error', console.error))
+    .pipe(rename('style.css'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream())
