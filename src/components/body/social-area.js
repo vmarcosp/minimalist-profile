374e764da2b270f0f2b388f598cf9acc64a87033
luanraithz <luanraithz@gmail.com>
2018-09-11 11:05:58 -0300

    importing font-awesome and refactoring component

--- src/components/body/social-area.js
+++ src/components/body/social-area.js
@@ -1,26 +1,38 @@
 import React from "react";
-import styled from 'styled-components';
-import { pink, grayLight } from '../../styleguide/colors';
+import styled from "styled-components";
+import { pink, grayLight } from "../../styleguide/colors";
 
 const StyledSocialArea = styled.div`
-  padding:2rem;
-  font-size:35px;
-  margin:0 auto;
-`
+  padding: 2rem;
+  font-size: 35px;
+  margin: 0 auto;
+`;
 
 const StyledMedia = styled.a`
-  margin: 0 .5rem;
-  &:active,&:link,&:visited{
-    color:${grayLight};
-    text-decoration:none;
-    outline:none;
+  margin: 0 0.5rem;
+  &:active,
+  &:link,
+  &:visited {
+    color: ${grayLight};
+    text-decoration: none;
+    outline: none;
   }
-  &:hover{
-    color:${pink};
-    cursor:pointer;
+  &:hover {
+    color: ${pink};
+    cursor: pointer;
   }
-`
+`;
 
-const Media = ({icon, link}, index) => <StyledMedia role="link" target="_blank" key={index} className={`icon fa ${icon}`} href={link}></StyledMedia>
-
-export const SocialArea = ({ socialLinks }) => <StyledSocialArea>{socialLinks.map(Media)}</StyledSocialArea>
+export const SocialArea = ({ socialLinks }) => (
+  <StyledSocialArea>
+    {socialLinks.map(({ icon, link }, index) => (
+      <StyledMedia
+        role="link"
+        target="_blank"
+        key={index}
+        className={`icon fa ${icon}`}
+        href={link}
+      />
+    ))}
+  </StyledSocialArea>
+);
